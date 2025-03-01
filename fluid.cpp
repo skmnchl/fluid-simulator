2263172f929ae513369b7d60c6e572f70325595b
skmnchl <skmnchl@gmail.com>
2025-03-01 10:54:38 +0900

    feat: Add Atom simulating function interacts with solid cells
    Add an Atom simulating function that correctly interacts with solid cells.
    (Atoms now correctly hit solid cells without passing or ignoring them)
    
    Add efficient collision detection function using spacial hash.
    (Nevertheless, it is not used in current simulator due to its slow speed)

--- fluid.cpp
+++ fluid.cpp
@@ -34,8 +34,8 @@ class Grid {
     std::vector<CellType> cell;
     std::vector<float> vel[2]; // grid velocity
     std::vector<float> prev_vel[2]; // grid previous velocity
-    std::vector<bool> s;
-    std::vector<float> r;
+    std::vector<bool> s; // true if fluid can be filled
+    std::vector<float> r; // total sum of weights
 
     Grid(std::vector<std::vector<Grid::CellType>> cell) {
         assert(cell.size() > 0 && cell[0].size() > 0);
@@ -150,7 +150,8 @@ class FlipFluid {
     int width;
     Atom atom = Atom({},{});
     Grid grid = Grid({{Grid::CellType::AIR}});
-    std::vector<int> cell_atom;
+    std::vector<int> num_cell_atom;
+    std::vector<int> cell_atom_idx;
     std::vector<float> density; // grid density
 
     float gravity;
@@ -184,7 +185,8 @@ class FlipFluid {
         this->atom = atom;
 
         // init others
-        this->cell_atom.resize(height * width * 10);
+        this->num_cell_atom.resize(height * width + 1);
+        this->cell_atom_idx.resize(atom.cnt);
         this->density.resize(height * width);
         this->gravity = gravity;
         this->dt = dt;
@@ -196,9 +198,14 @@ class FlipFluid {
         this->rest_density = rest_density;
     }
 
-    void reset_cell_atom() {
-        for(int i=0;i<cell_atom.size();i++)
-            cell_atom[i] = 0;
+    void reset_num_cell_atom() {
+        for(int i=0;i<num_cell_atom.size();i++)
+            num_cell_atom[i] = 0;
+    }
+    
+    void reset_cell_atom_idx() {
+        for(int i=0;i<cell_atom_idx.size();i++)
+            cell_atom_idx[i] = 0;
     }
 
     void reset_density() {
@@ -207,68 +214,134 @@ class FlipFluid {
     }
 
     void simulate_atom(float gravity, float dt) {
+        int step = 10;
         for(int a=0;a<atom.cnt;a++) {
-            atom.vel[a*2 + 1] += gravity * dt;
-            atom.pos[a*2] += atom.vel[a*2] * dt;
-            atom.pos[a*2 + 1] += atom.vel[a*2 + 1] * dt;
+            atom.vel[a*2 + 1] += gravity * dt; // add gravity
+
+            float dx = atom.vel[a*2] * dt / step;
+            float dy = atom.vel[a*2 + 1] * dt / step;
+            float x0 = atom.pos[a*2];
+            float y0 = atom.pos[a*2 + 1];
+            float x = x0;
+            float y = y0;
+            float rx = dx>0 ? min_dist : -min_dist;
+            float ry = dy>0 ? min_dist : -min_dist;
+            for(int i=0;i<step;i++) {
+                if (x>=width || x<=0 || y>=height || y<=0) {
+                    break;
+                }
+
+                if (grid.cell[floor(y+ry) * width + floor(x+rx)] == Grid::CellType::SOLID) {
+                    if (dx == 0) {
+                        atom.vel[a*2 + 1] = 0;
+                        break;
+                    } else if (dy == 0) {
+                        atom.vel[a*2] = 0;
+                        break;
+                    }
+
+                    float corner_x = floor(x) + (dx>0 ? -min_dist : 1+min_dist);
+                    float corner_y = floor(y) + (dy>0 ? -min_dist : 1+min_dist);
+                    float cross_product = (corner_x - x0) * (y - y0) - (corner_y - y0) * (x - x0);
+                    float direction = -cross_product * dx * dy;
+                    if (direction > 0) {
+                        atom.vel[a*2] = 0;
+                    } else if (direction < 0) {
+                        atom.vel[a*2 + 1] = 0;
+                    } else {
+                        atom.vel[a*2] = 0;
+                        atom.vel[a*2 + 1] = 0;
+                    }
+
+                    break;
+                }
+
+                x += dx;
+                y += dy;
+            }
+
+            atom.pos[a*2] = x;
+            atom.pos[a*2 + 1] = y;
         }
     }
 
-    // void push_atoms_apart(float min_dist, int num_iter) {
-    //     // use grid method
-    //     // divide each atom into grid
-    //     reset_cell_atom();
-    //     // consider near 9 cells
-    //     for(int i=0;i<atom.cnt;i++) {
-    //         int a_x = floor(atom.pos[i*2]);
-    //         int a_y = floor(atom.pos[i+2 + 1]);
-    //         int cell_x[3] = {a_x-1, a_x, a_x+1};
-    //         int cell_y[3] = {a_y-1, a_y, a_y+1};
-    //         for(int i=0;i<3;i++) {
-    //             for(int j=0;j<3;j++) {
-    //                 if (cell_x[j]<0 || cell_x[j]>=width || cell_y[i]<0 || cell_y[i]>=height)
-    //                     continue;
-    //                 // int &idx = cell_atom[cell_y[i]][cell_x[j]][0];
-    //                 // cell_atom[cell_y[i]][cell_x[j]][idx] = i;
-    //                 int &idx = cell_atom[(cell_y[i]*height + cell_x[j])*width];
-    //                 cell_atom[(cell_y[i]*height + cell_x[j])*width + idx] = i;
-    //                 idx += 1;
-    //             }
-    //         }
-    //     }
+    void push_atoms_apart(float min_dist, int num_iter) {
+        // use grid method
+        // divide each atom into grid
+        reset_num_cell_atom();
+        reset_cell_atom_idx();
+        
+        // count number of atoms in each cell 
+        for(int a=0;a<atom.cnt;a++) {
+            float xi = floor(atom.pos[a*2]);
+            float yi = floor(atom.pos[a*2 + 1]);
+            if (yi>=height || yi<0 || xi>=height || xi<0)
+                continue;
+            num_cell_atom[yi * width + xi] += 1;
+        }
 
-    //     for(int iter=0;iter<num_iter;iter++) {
-    //         // consider near 9 cells
-    //         for(int i=0;i<height;i++) {
-    //             for(int j=0;j<width;j++) {
-    //                 // see if atoms collide in each cell
-    //                 // int idx = cell_atom[i][j][0];
-    //                 int idx = cell_atom[(i*height + j)*width];
-    //                 for(int p=0;p<idx;p++) {
-    //                     for(int q=p+1;q<idx;q++) {
-    //                         Atom &a1 = atom[p];
-    //                         Atom &a2 = atom[q];
-    //                         float dist = sqrt(pow(a1.pos[0]-a2.pos[0],2)+pow(a1.pos[1]-a2.pos[1],2));
-    //                         if (dist < min_dist) {
-    //                             float mid_x = (a1.pos[0]+a2.pos[0]) / 2;
-    //                             float mid_y = (a1.pos[1]+a2.pos[1]) / 2;
+        // make partial sum of number of atoms in each cell
+        int atom_cnt = 0;
+        for(int i=0;i<num_cell_atom.size();i++) {
+            atom_cnt += num_cell_atom[i];
+            num_cell_atom[i] = atom_cnt;
+        }
 
-    //                             if (dist == 0) {
-    //                                 continue;
-    //                             }
+        // fill atom idx in seperate vector
+        for(int a=0;a<atom.cnt;a++) {
+            float xi = floor(atom.pos[a*2]);
+            float yi = floor(atom.pos[a*2 + 1]);
+            if (yi>=height || yi<0 || xi>=height || xi<0)
+                continue;
+            num_cell_atom[yi * width + xi] -= 1;
+            cell_atom_idx[num_cell_atom[yi * width + xi]] = a;
+        }
 
-    //                             a1.pos[0] = (a1.pos[0]-mid_x) * min_dist / dist;
-    //                             a2.pos[0] = (a2.pos[0]-mid_x) * min_dist / dist;
-    //                             a1.pos[1] = (a1.pos[1]-mid_y) * min_dist / dist;
-    //                             a2.pos[1] = (a2.pos[1]-mid_y) * min_dist / dist;
-    //                         }
-    //                     }
-    //                 }
+        // push atoms apart
+        for(int iter=0;iter<num_iter;iter++) {
+            for(int a1=0;a1<atom.cnt;a1++) {
+                // consider near 9 cells
+                float px = atom.pos[2*a1];
+                float py = atom.pos[2*a1 + 1];
+                int xi = floor(px);
+                int yi = floor(py);
+                int x0 = (xi-1>=0) ? xi-1 : 0;
+                int x1 = (xi+1<width) ? xi+1 : width-1;
+                int y0 = (yi-1>=0) ? yi-1 : 0;
+                int y1 = (yi+1<height) ? yi+1 : height-1;
 
-    //             }
-    //         }
-    //     }
-    // }
+                for(int y=y0;y<=y1;y++) {
+                    for(int x=x0;x<=x1;x++) {
+                        int cell_num = y * width + x;
+                        int first = num_cell_atom[cell_num];
+                        int last = num_cell_atom[cell_num + 1];
+                        for (int atom_id = first; atom_id < last; atom_id++) {
+                            int a2 = cell_atom_idx[atom_id];
+                            if (a2 == a1)
+                                continue;
+                            float qx = atom.pos[2*a2];
+                            float qy = atom.pos[2*a2 + 1];
+
+                            float dx = qx - px;
+                            float dy = qy - py;
+                            // float dist = sqrt(pow(dx, 2) + pow(dy, 2)); -> this is double slower
+                            float dist = std::sqrt(dx*dx + dy*dy);
+                            if (dist > min_dist || dist == 0.0)
+                                continue;
+                            float separate = 0.5 * (min_dist - dist) / dist;
+                            dx *= separate;
+                            dy *= separate;
+                            
+                            atom.pos[2*a1] -= dx;
+                            atom.pos[2*a1 + 1] -= dy;
+                            atom.pos[2*a2] += dx;
+                            atom.pos[2*a2 + 1] += dy;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     void handle_atom_collisions(float min_dist) {
         for(int a=0;a<atom.cnt;a++) {
@@ -458,8 +531,8 @@ class FlipFluid {
         // simulate particles
         simulate_atom(gravity, dt);
 
-        // push_atoms_apart(min_dist, pushapart_iteration);
         handle_atom_collisions(min_dist);
+        push_atoms_apart(min_dist, pushapart_iteration);
 
         // grid.print_grid_status("\n\ninit grid:\n");
 
@@ -474,7 +547,7 @@ class FlipFluid {
 
         // save current grid velocity
         for(int d=0;d<2;d++)
-            grid.prev_vel[d] = grid.vel[d];
+            grid.vel[d].swap(grid.prev_vel[d]);
         
         // make fluid incompressible
         // using Gauss-Seidal method
@@ -510,6 +583,13 @@ int main() {
     for(int i=0;i<width;i++) {
         init_cell[0][i] = Grid::CellType::SOLID;
         init_cell[height-1][i] = Grid::CellType::SOLID;
+    }
+    for(int i=height*3/4;i<height*15/16;i++) {
+        init_cell[i][width*3/4] = Grid::CellType::SOLID;
+        init_cell[i][width/4] = Grid::CellType::SOLID;
+    }
+    for(int i=width/4;i<width*3/4;i++) {
+        init_cell[height*15/16][i] = Grid::CellType::SOLID;
     }
     Grid grid = Grid(init_cell);
 
